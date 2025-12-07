4e0d1017a0522900f233c817668fe9ef2460b6f4
Wishant010 <144028795+Wishant010@users.noreply.github.com>
2025-12-08 00:56:17 +0100

    fix domein naam  voor .xml

--- woningspotters/src/app/sitemap.ts
+++ woningspotters/src/app/sitemap.ts
@@ -2,7 +2,7 @@
 import type { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://woningspotters.nl'
+  const baseUrl = 'https://woningspotter.nl'
 
   return [
     {
